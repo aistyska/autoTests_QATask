5e6c9341f32811aad7d06d23fb712d9d45a047f4
Aiste Laugayte <aiste.laugalyte@gmail.com>
2022-05-01 15:11:00 +0300

    Created tests for the first game and to check if new grid have an appropriate number of columns and rows

---
+++ tests/firstGame.test.js
@@ -0,0 +1,64 @@
+const puppeteer = require('puppeteer');
+const assert = require('chai').assert;
+
+describe("Play game for the first time. Grid size 4x4", () => {
+    let browser;
+    let page;
+
+    before("Open the game", async function() {
+        browser = await puppeteer.launch({headless: false});
+        page = await browser.newPage();
+        await page.goto("file:///C:/Users/aiste/Desktop/qa%20Task/QA%20Task.html");
+    });
+
+    it("Click all icons in the outer perimeter and define size of a new grid", async function() {
+        page.on('dialog', async dialog => {
+            const message = dialog.message();
+            assert.equal(message, "Done! Ready for the next try? Give me a size [3-9]:")
+            await dialog.accept("3");
+        });
+
+        const header = await page.waitForSelector(".main h1");
+        const headerText = await header.evaluate(element => element.textContent);
+        assert.equal(headerText, "Play it is fun !!!");
+
+        const iconsInFirstLine = await page.$$("div.mainGrid div.row:first-of-type > div:nth-child(-n+4)");
+        for (let index = 0; index < iconsInFirstLine.length; index++) {
+            await iconsInFirstLine[index].click();
+        }
+     
+        const iconsInLastLine = await page.$$("div.mainGrid div.row:last-of-type > div:nth-child(-n+4)");
+        for (let index = 0; index < iconsInLastLine.length; index++) {
+            await iconsInLastLine[index].click();
+        }
+
+        const firstIconIn2ndLine = await page.$("div.mainGrid div.row:nth-of-type(2) div:first-child");
+        await firstIconIn2ndLine.click();
+
+        const firstIconIn3rdLine = await page.$("div.mainGrid div.row:nth-of-type(3) div:first-child");
+        await firstIconIn3rdLine.click();
+
+        const lastIconIn2ndLine = await page.$("div.mainGrid div.row:nth-of-type(2) div:nth-last-child(2)");
+        await lastIconIn2ndLine.click();
+
+        let clickedIcons = await page.$$(".mainGrid div[active=true]");
+        assert.equal(clickedIcons.length, 11);
+
+        const lastIconIn3rdLine = await page.$("div.mainGrid div.row:nth-of-type(3) div:nth-last-child(2)");
+        await lastIconIn3rdLine.click();
+
+// Page refreshes after new size is entered in prompt
+        await page.waitForNavigation();
+        
+        const displayedIcons = await page.$$("div.mainGrid div.icon");
+        assert.equal(displayedIcons.length, 9);
+        
+        clickedIcons = await page.$$(".mainGrid div[active=true]");
+        assert.equal(clickedIcons.length, 0);
+    });
+
+
+    after("Close Browser", async function() {
+        await browser.close();
+    });
+})
---
+++ tests/newGrid.test.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer');
+const assert = require('chai').assert;
+
+describe("New grid after prompt", () => {
+    let browser;
+    let page;
+
+    before("Launch browser", async function() {
+        browser = await puppeteer.launch({headless: false});
+        page = await browser.newPage();
+    });
+
+    const grids = [
+        {"size": "3", "cells": 9}, 
+        {"size": "4", "cells": 16}, 
+        {"size": "5", "cells": 25}, 
+        {"size": "6", "cells": 36}, 
+        {"size": "7", "cells": 49}, 
+        {"size": "8", "cells": 64},
+        {"size": "9", "cells": 81}
+    ];
+    index = 0;
+
+    beforeEach("Opent the game and click all icons in outter perimeter to see the promt", async function() {
+        await page.goto("file:///C:/Users/aiste/Desktop/qa%20Task/QA%20Task.html");
+        page.on('dialog', async dialog => {
+            await dialog.accept(grids[index].size);
+        });
+
+        await page.waitForSelector("div.mainGrid div.row:first-of-type > div:nth-child(-n+4)");
+        const iconsInFirstLine = await page.$$("div.mainGrid div.row:first-of-type > div:nth-child(-n+4)");
+        for (let index = 0; index < iconsInFirstLine.length; index++) {
+            await iconsInFirstLine[index].click();
+        }
+    
+        const iconsInLastLine = await page.$$("div.mainGrid div.row:last-of-type > div:nth-child(-n+4)");
+        for (let index = 0; index < iconsInLastLine.length; index++) {
+            await iconsInLastLine[index].click();
+        }
+
+        const firstIconIn2ndLine = await page.$("div.mainGrid div.row:nth-of-type(2) div:first-child");
+        await firstIconIn2ndLine.click();
+
+        const firstIconIn3rdLine = await page.$("div.mainGrid div.row:nth-of-type(3) div:first-child");
+        await firstIconIn3rdLine.click();
+
+        const lastIconIn2ndLine = await page.$("div.mainGrid div.row:nth-of-type(2) div:nth-last-child(2)");
+        await lastIconIn2ndLine.click();
+
+        const lastIconIn3rdLine = await page.$("div.mainGrid div.row:nth-of-type(3) div:nth-last-child(2)");
+        await lastIconIn3rdLine.click();
+
+        index++
+    });
+
+    grids.forEach(function(grid) {
+        it(`Verify grid with ${grid.size} columns and rows is displayed`, async function() {
+            await page.waitForNavigation();
+            const clickedIcons = await page.$$(".mainGrid div[active=true]");
+            assert.equal(clickedIcons.length, 0, "Clicked icons exist");
+
+            const displayedIcons = await page.$$("div.mainGrid div.icon");
+            assert.equal(displayedIcons.length, grid.cells, "Not all icons are displayed");
+            
+            const displayedRows = await page.$$("div.mainGrid div.row");
+            assert.equal(displayedRows.length, grid.size, "Wrong number of rows is displayed");
+
+            for (let row = 1; row <= 3; row++) {
+                let columnsInRow = await page.$$(`div.mainGrid div.row:nth-of-type(${row}) div.icon`);
+                assert.equal(columnsInRow.length, grid.size, `Wrong number of columns in the ${row} row`)
+            };
+        });
+    });
+
+    after("Close Browser", async function() {
+        await browser.close();
+    });
+})
